b0c442d0d05edef1b8925d884ea18198ad33d05d
Alex Rogach <alex.rogach131@ukr.net>
2022-08-03 14:26:39 +0300

    module 7

---
+++ js/02-lightbox.js
@@ -0,0 +1,23 @@
+import { galleryItems } from "./gallery-items.js";
+// Change code below this line
+
+const $gallery = document.querySelector(".gallery");
+
+let galleryItemsArr = [...galleryItems].map((item) => {
+  return `
+    <a class="gallery__item" href="${item.original}">
+      <img
+        class=gallery__image
+        src=${item.preview}
+        alt=${item.description}
+      />
+    </a>
+  `;
+});
+
+$gallery.innerHTML = [...galleryItemsArr].join("");
+
+new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
